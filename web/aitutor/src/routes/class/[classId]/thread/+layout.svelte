adc5879bdf3da7a72f25755385b2b6c3092816e2
Joe Nudell <jnu@stanford.edu>
2023-12-05 21:31:23 -0800

    styling

--- web/aitutor/src/routes/class/[classId]/thread/+layout.svelte
+++ web/aitutor/src/routes/class/[classId]/thread/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   // Import necessary components from Flowbite-Svelte
   import { Input, Button } from 'flowbite-svelte';
+  import {enhance} from '$app/forms';
 
   export let data;
 
@@ -9,13 +10,34 @@
   const formUrl = data.currentThread ?
     `/class/${data.class.id}/thread/${data.currentThread}?/newMessage` :
     `/class/${data.class.id}/thread?/newThread`;
+
+  let thread = [];
+  let threads = [];
+
+  $: {
+    threads = data?.class?.threads || [];
+    threads.sort((a, b) => {
+      return new Date(b.created).getTime() - new Date(a.created).getTime();
+    });
+
+    thread = data?.thread?.data || [];
+  }
+
+  const scrollToBottom = (node) => {
+    const scroll = () => node.scroll({
+      top: node.scrollHeight,
+      behavior: 'smooth'
+    });
+    scroll();
+    return {update: scroll};
+  };
 </script>
 
 <div class="flex h-screen">
-    <div class="w-1/5 overflow-y-auto p-4 bg-sky-800">
+    <div class="basis-64 grow-0 shrink-0 overflow-y-auto p-4 bg-sky-800">
         <!-- List of chat threads -->
         <ul>
-            {#each data.class.threads as thread}
+            {#each threads as thread}
                 <li
                     class="p-2 hover:bg-sky-700 cursor-pointer rounded"
                     class:bg-sky-700={thread.id === data.currentThread}
@@ -28,12 +50,12 @@
             {/each}
         </ul>
     </div>
-    <div class="flex-grow flex flex-col">
-        <div class="flex-grow overflow-y-auto p-4 bg-white">
+    <div class="flex flex-col flex-grow">
+        <div class="flex-grow overflow-y-auto p-4 bg-white" use:scrollToBottom={thread}>
             <!-- Selected conversation -->
             <slot></slot>
         </div>
-        <form action={formUrl} method="POST">
+        <form action={formUrl} method="POST" use:enhance>
         <div class="flex items-center border-t border-gray-300 p-4">
             <Input placeholder="Type a message..." name="message" id="message" class="w-full mr-2" />
             <Button type="submit" color="dark">Send</Button>
--- web/aitutor/src/routes/class/[classId]/thread/[threadId]/+page.svelte
+++ web/aitutor/src/routes/class/[classId]/thread/[threadId]/+page.svelte
@@ -1,19 +1,27 @@
 <script lang="ts">
+  import {Avatar} from "flowbite-svelte";
   import SvelteMarkdown from "svelte-markdown";
 
   export let data;
 
-  const thread = (data?.thread?.data || []).reverse();
+  let thread = [];
 
-  console.log(thread)
+  $: {
+    thread = (data?.thread?.data || []);
+    thread.sort((a, b) => a.created_at - b.created_at);
+  }
 </script>
 
-<div class="w-full divide-y">
+<div class="w-full px-2">
   {#each thread as message}
-    <div>
-    {#each message.content as content}
-      <div><SvelteMarkdown source="{content.text.value}" /></div>
-    {/each}
+    <div class="py-4 px-6 flex gap-x-3">
+      <div><Avatar size="xs" /></div>
+      <div>
+        <div class="font-bold text-gray-400">{message.role}</div>
+        {#each message.content as content}
+          <div class="leading-7"><SvelteMarkdown source="{content.text.value}" /></div>
+        {/each}
+      </div>
     </div>
   {/each}
 </div>
