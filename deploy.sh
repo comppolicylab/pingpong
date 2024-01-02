698590fafdc9b3603127d4d7ed71634a38fea8f2
Joe Nudell <jnudell@hks.harvard.edu>
2024-01-02 15:11:29 -0800

    finish deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -e
+
+source ./docker.env
+docker login --username $AITUTOR_DOCKER_USER --password $AITUTOR_DOCKER_PW aitutor.azurecr.io
+
+docker compose -f docker-compose.yml -f docker-compose.prod.yml build
+docker push aitutor.azurecr.io/srv:latest
+docker push aitutor.azurecr.io/web:latest
+
+scp docker.env aitutor:~/
+scp docker-compose.yml aitutor:~/
+scp docker-compose.prod.yml aitutor:~/
+scp config.prod.toml aitutor:~/
+scp nginx.conf aitutor:~/
+scp cert/aitutor.hks.harvard.edu.cer aitutor:~/
+scp cert/aitutor.hks.harvard.edu.key aitutor:~/
+ssh -T aitutor << EOF
+  source ./docker.env
+  sudo mkdir -p /opt/aitutor
+  sudo mkdir -p /opt/aitutor/cert
+  sudo mkdir -p /opt/aitutor/db
+  sudo cp docker-compose.yml /opt/aitutor/
+  sudo cp docker-compose.prod.yml /opt/aitutor/
+  sudo cp config.prod.toml /opt/aitutor/
+  sudo cp nginx.conf /opt/aitutor/
+  sudo cp aitutor.hks.harvard.edu.cer /opt/aitutor/cert/
+  sudo cp aitutor.hks.harvard.edu.key /opt/aitutor/cert/
+
+  cd /opt/aitutor
+  sudo systemctl start docker
+  sudo docker login --username $AITUTOR_DOCKER_USER --password $AITUTOR_DOCKER_PW aitutor.azurecr.io
+  sudo docker compose -f docker-compose.yml -f docker-compose.prod.yml down
+  sudo docker compose -f docker-compose.yml -f docker-compose.prod.yml pull
+  sudo docker compose -f docker-compose.yml -f docker-compose.prod.yml up --no-build -d
+EOF
