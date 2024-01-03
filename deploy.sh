0b5da416a31a3b23e5e21e4d0b376b3a54b5ccce
Joe Nudell <jnudell@hks.harvard.edu>
2024-01-03 14:05:57 -0800

    create user and group in deploy script

--- deploy.sh
+++ deploy.sh
@@ -18,15 +18,23 @@ scp cert/aitutor.hks.harvard.edu.cer aitutor:~/
 scp cert/aitutor.hks.harvard.edu.key aitutor:~/
 ssh -T aitutor << EOF
   source ./docker.env
+
+  sudo useradd -m -s /bin/bash aitutor || true
+  sudo groupadd -f aitutor
+  sudo usermod -aG aitutor aitutor
+
   sudo mkdir -p /opt/aitutor
   sudo mkdir -p /opt/aitutor/cert
   sudo mkdir -p /opt/aitutor/db
+
   sudo cp docker-compose.yml /opt/aitutor/
   sudo cp docker-compose.prod.yml /opt/aitutor/
   sudo cp config.prod.toml /opt/aitutor/
   sudo cp nginx.conf /opt/aitutor/
   sudo cp aitutor.hks.harvard.edu.cer /opt/aitutor/cert/
   sudo cp aitutor.hks.harvard.edu.key /opt/aitutor/cert/
+
+  sudo chown -R aitutor:aitutor /opt/aitutor
 
   cd /opt/aitutor
   sudo systemctl start docker
