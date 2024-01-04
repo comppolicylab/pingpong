45ebcd687c438d5c5240be8e32235e7fcf3bcd71
Joe Nudell <jnudell@hks.harvard.edu>
2024-01-03 23:33:19 -0800

    some copy for about page (#71)

--- web/aitutor/src/routes/about/+page.svelte
+++ web/aitutor/src/routes/about/+page.svelte
@@ -1,3 +1,10 @@
+<script lang="ts">
+  import {Heading} from "flowbite-svelte";
+  // TODO - way to give feedback
+</script>
+<div class="mx-8 my-4 flex flex-col gap-2">
+  <Heading tag="h2">About</Heading>
 <div>
-  TKTK - about content!
+This app was developed by the <a href="https://policylab.hks.harvard.edu">Computational Policy Lab</a> at the Harvard Kennedy School.
+</div>
 </div>
