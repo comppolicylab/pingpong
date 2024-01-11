869325293aa21fe28b6f23701c1fd8c657c298e6
Joe Nudell <jnudell@hks.harvard.edu>
2024-01-10 18:52:42 -0800

    style improvements for textarea

--- web/aitutor/src/lib/components/ChatInput.svelte
+++ web/aitutor/src/lib/components/ChatInput.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import {ButtonGroup, Textarea, GradientButton} from "flowbite-svelte";
   import {page} from "$app/stores";
+  import {ChevronUpSolid} from 'flowbite-svelte-icons';
 
   export let loading = false;
+  export let maxHeight = 200;
+  export let defaultHeight = 40;
 
   // Focus textarea when component is mounted. Since we can only use `use` on
   // native DOM elements, we need to wrap the textarea in a div and then
@@ -16,8 +19,15 @@
     };
   };
 
+  const fixHeight = (e) => {
+    const el = e.target;
+    el.style.height = 0;
+    const scrollHeight = el.scrollHeight;
+    el.style.height = `${scrollHeight + 2}px`;
+  };
+
   // Submit form when Enter (but not Shift+Enter) is pressed in textarea
-  const checkKey = (e) => {
+  const maybeSubmit = (e) => {
     if (e.key === "Enter" && !e.shiftKey) {
       e.preventDefault();
       e.target.form.requestSubmit();
@@ -25,9 +35,10 @@
   };
 </script>
 
-<ButtonGroup class="w-full">
-  <div use:init={$page.params.threadId} class="w-full">
-    <Textarea id="message" rows="1" name="message" placeholder="Ask me anything" disabled={loading} on:keydown={checkKey} />
-  </div>
-  <GradientButton type="submit" color="cyanToBlue" class={loading ? "animate-pulse cursor-progress" : undefined}>Send</GradientButton>
-</ButtonGroup>
+<div use:init={$page.params.threadId} class="w-full relative">
+  <Textarea id="message" rows="1" name="message" placeholder="Ask me anything" disabled={loading} on:keydown={maybeSubmit} on:input={fixHeight}
+                                                 style={`max-height: ${maxHeight}px; padding-right: 3rem; font-size: 1rem; line-height: 1.5rem;`}
+    class="resize-none"
+    />
+    <GradientButton type="submit" color="cyanToBlue" class={`${loading ? "animate-pulse cursor-progress" : ""} p-2 absolute bottom-3 right-2.5`}><ChevronUpSolid size="xs" /></GradientButton>
+</div>
