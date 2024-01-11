671b52a7e3141824a9d263a723bfd1be18675d53
Joe Nudell <jnudell@hks.harvard.edu>
2024-01-10 17:53:15 -0800

    fix focus and shift-enter behavior

--- web/aitutor/src/lib/components/ChatInput.svelte
+++ web/aitutor/src/lib/components/ChatInput.svelte
@@ -4,19 +4,30 @@
 
   export let loading = false;
 
+  // Focus textarea when component is mounted. Since we can only use `use` on
+  // native DOM elements, we need to wrap the textarea in a div and then
+  // access its child to imperatively focus it.
   const init = (el) => {
-    el.focus();
+    el.children[0].focus();
     return {
       update: () => {
-        el.focus();
+        el.children[0].focus();
       },
     };
+  };
+
+  // Submit form when Enter (but not Shift+Enter) is pressed in textarea
+  const checkKey = (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      e.target.form.requestSubmit();
+    }
   };
 </script>
 
 <ButtonGroup class="w-full">
-  <Textarea id="message" rows="1" name="message" placeholder="Ask me anything" let:props>
-    <textarea use:init={$page.params.threadId} disabled={loading} {...props} />
-  </Textarea>
+  <div use:init={$page.params.threadId} class="w-full">
+    <Textarea id="message" rows="1" name="message" placeholder="Ask me anything" disabled={loading} on:keydown={checkKey} />
+  </div>
   <GradientButton type="submit" color="cyanToBlue" class={loading ? "animate-pulse cursor-progress" : undefined}>Send</GradientButton>
 </ButtonGroup>
