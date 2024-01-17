1790a1c3113680dee7c79f3065bad3e8b7417262
Joe Nudell <jnudell@hks.harvard.edu>
2024-01-17 12:57:56 -0800

    refactor email settings (#102)

---
+++ aitutor/email/base.py
@@ -0,0 +1,8 @@
+from abc import abstractmethod
+from typing import Protocol
+
+
+class EmailSender(Protocol):
+    @abstractmethod
+    async def send(self, to: str, subject: str, message: str):
+        ...
---
+++ aitutor/invite.py
@@ -0,0 +1,21 @@
+from .email import EmailSender
+from .schemas import CreateInvite
+
+
+async def send_invite(
+    sender: EmailSender, invite: CreateInvite, link: str, silent: bool = False
+):
+    """Send an email invitation for a user to join a class."""
+    subject = f"You've been invited to join {invite.class_name}!"
+    message = f"""\
+Hello! You've been invited to join {invite.class_name} on AI Tutor. \
+To join, click the link below:
+
+{link}\
+"""
+
+    if silent:
+        print(f"Would have sent email to {invite.email} with subject {subject}")
+        print(message)
+        return
+    await sender.send(invite.email, subject, message)
