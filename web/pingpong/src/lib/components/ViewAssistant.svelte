66bbf57c3a8fc920d91d057e5811048372dd5fe7
Joe Nudell <jnudell@hks.harvard.edu>
2024-01-18 11:02:53 -0800

    add another little link to the assistant from the manage page (#110)

--- web/pingpong/src/lib/components/ViewAssistant.svelte
+++ web/pingpong/src/lib/components/ViewAssistant.svelte
@@ -1,22 +1,43 @@
 <script lang="ts">
-  import {Heading, Label, List, Li} from "flowbite-svelte";
+  import {page} from "$app/stores";
+  import {copy} from "svelte-copy";
+  import {toast} from "@zerodevx/svelte-toast";
+  import {Heading, Label, Button, List, Li} from "flowbite-svelte";
   import {
     EyeOutline,
     EyeSlashOutline,
+    LinkOutline,
   } from 'flowbite-svelte-icons';
 
   export let assistant;
   export let creator;
+
+  // Get the full URL to use the assistant
+  $: assistantLink = `${$page.url.protocol}//${$page.url.host}/class/${assistant.class_id}?assistant=${assistant.id}`;
+
+  // Show info that we copied the link to the clipboard
+  const showCopiedLink = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    toast.push("Copied link to clipboard", {
+      duration: 1000,
+    });
+  };
 </script>
 
 <div class="flex flex-col gap-2">
-  <Heading tag="h4" class="pb-3">
+  <Heading tag="h4" class="pb-3 flex justify-between">
+    <div>
     {#if !assistant.published}
     <EyeSlashOutline class="inline-block w-6 h-6 mr-2 text-gray-300" />
     {:else}
     <EyeOutline class="inline-block w-6 h-6 mr-2 text-amber-600" />
     {/if}
-    {assistant.name}</Heading>
+    {assistant.name}
+    </div>
+
+    <button on:click|preventDefault={() => {}} on:svelte-copy={showCopiedLink} use:copy={assistantLink}><LinkOutline class="inline-block w-6 h-6 text-gray-700 hover:text-green-700 active:animate-ping" /></button>
+  </Heading>
   <Label>Author</Label>
   <span>{creator.email}</span>
   <Label>Instructions</Label>
--- web/pingpong/src/routes/+layout.svelte
+++ web/pingpong/src/routes/+layout.svelte
@@ -2,10 +2,12 @@
   import "../app.pcss";
   import Sidebar from '$lib/components/Sidebar.svelte';
   import Main from '$lib/components/Main.svelte';
+  import { SvelteToast } from '@zerodevx/svelte-toast';
 
   export let data;
 </script>
 
+<SvelteToast />
 {#if data.me.user}
   <div class="flex h-screen">
     <Sidebar {data} />
@@ -16,3 +18,12 @@
 {:else}
   <slot></slot>
 {/if}
+
+
+<style lang="css">
+  :root {
+    --toastBackground: #22c55e;
+    --toastBorderRadius: 0.5rem;
+    --toastBarBackground: #1d9e48;
+  }
+</style>
