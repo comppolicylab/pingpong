901f0064d4566d3aa3a11ac5b6668a4ee04ba468
Joe Nudell <jnudell@hks.harvard.edu>
2024-01-21 23:56:26 -0800

    add sentry to node

--- web/pingpong/src/hooks.server.ts
+++ web/pingpong/src/hooks.server.ts
@@ -1,4 +1,14 @@
+import {sequence} from '@sveltejs/kit/hooks';
+import * as Sentry from '@sentry/sveltekit';
 import {API_HOST, API_PROTO} from '$env/static/private';
+
+const SENTRY_DSN = process.env.SENTRY_DSN;
+if (SENTRY_DSN) {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    tracesSampleRate: 1
+  });
+}
 
 export async function handleFetch({ request, fetch, event }) {
   // The server needs to redirect API requests to the API server, keeping
@@ -13,3 +23,5 @@ export async function handleFetch({ request, fetch, event }) {
 
   return fetch(request);
 }
+export const handleError = Sentry.handleErrorWithSentry();
+export const handle = sequence(Sentry.sentryHandle());
