d6a7b658142bd502f0a18a04e9a410c5522d5bb4
Joe Nudell <jnudell@hks.harvard.edu>
2024-01-26 11:47:58 -0800

    validation for rollout script

--- rollout.sh
+++ rollout.sh
@@ -5,6 +5,18 @@ set -e
 # Get deployment name from CLI; it is the first argument
 DEPLOYMENT_NAME=$1
 
+# Bail if no deployment name is given
+if [ -z "$DEPLOYMENT_NAME" ]; then
+  echo "No deployment name given."
+  exit 1
+fi
+
+# Bail if deployment is not `srv` or `web`
+if [ "$DEPLOYMENT_NAME" != "srv" ] && [ "$DEPLOYMENT_NAME" != "web" ]; then
+  echo "Deployment name must be 'srv' or 'web'."
+  exit 1
+fi
+
 # Run the rollout command for the given DEPLOYMENT_NAME on the remote machine.
 ssh -T pingpong << EOF
   cd /opt/pingpong
