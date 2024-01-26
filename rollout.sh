98bb5acebeb79efcf4d7321db768e40f75cacf00
Joe Nudell <jnudell@hks.harvard.edu>
2024-01-26 11:43:07 -0800

    add pull to rollout script to make sure

--- rollout.sh
+++ rollout.sh
@@ -8,5 +8,6 @@ DEPLOYMENT_NAME=$1
 # Run the rollout command for the given DEPLOYMENT_NAME on the remote machine.
 ssh -T pingpong << EOF
   cd /opt/pingpong
+  sudo docker compose -f docker-compose.yml -f docker-compose.prod.yml pull $DEPLOYMENT_NAME
   sudo docker rollout -f docker-compose.yml -f docker-compose.prod.yml $DEPLOYMENT_NAME
 EOF
