9789c20b435f0a96c19317647588e4829a4441e4
Joe Nudell <jnudell@hks.harvard.edu>
2024-03-21 19:15:37 -0700

    add external rel to purified links

--- web/pingpong/src/lib/purify.ts
+++ web/pingpong/src/lib/purify.ts
@@ -4,7 +4,7 @@ import DOMPurify from 'isomorphic-dompurify';
 DOMPurify.addHook('afterSanitizeAttributes', function (node) {
   if (node?.tagName === 'A') {
     node.setAttribute('target', '_blank');
-    node.setAttribute('rel', 'noopener noreferrer');
+    node.setAttribute('rel', 'noopener noreferrer external');
   }
 });
 
