3dc69e51d850476837f59695786dbbf609d23da1
Joe Nudell <jnudell@hks.harvard.edu>
2024-03-29 15:47:30 -0700

    disable sentry fe

--- web/pingpong/vite.config.ts
+++ web/pingpong/vite.config.ts
@@ -1,15 +1,8 @@
-import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [
-    sentrySvelteKit({
-      sourceMapsUploadOptions: {
-        org: 'stanford-cpl',
-        project: 'javascript-sveltekit'
-      }
-    }),
     sveltekit()
   ],
   test: {
