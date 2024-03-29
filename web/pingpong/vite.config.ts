5a98b48f45e23246f7a3c8beeee8fc5bdeca3625
Joe Nudell <jnudell@hks.harvard.edu>
2024-03-29 16:38:27 -0700

    upgrade deps

--- web/pingpong/vite.config.ts
+++ web/pingpong/vite.config.ts
@@ -1,33 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
-const ClosePlugin = () => {
-  return {
-    name: 'ClosePlugin',
-    buildEnd: (error: Error | string | null | undefined) => {
-      if (error) {
-        console.error("Error during build:")
-        console.error(error);
-        process.exit(1);
-      } else {
-        console.log("Build finished!");
-        process.exit(0);
-      }
-    },
-    closeBundle() {
-      console.log('Closing bundle');
-      process.exit(0);
-    }
-  };
-
-}
-
-
 export default defineConfig({
-  plugins: [
-    sveltekit(),
-    ClosePlugin(),
-  ],
+  plugins: [sveltekit()],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}']
   },
