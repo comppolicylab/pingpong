a34d6f4c2905783ad09bcced60a52ae02bf6f4d4
Joe Nudell <jnudell@hks.harvard.edu>
2024-04-23 20:35:17 -0400

    refactor to move page header into one place

---
+++ web/pingpong/src/lib/components/PageHeader.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { Heading } from "flowbite-svelte";
+</script>
+
+<header class="bg-blue-light-50 p-8 pb-6">
+    <Heading tag="h2" class="font-serif">Welcome to PingPong!</Heading>
+</header>
--- web/pingpong/src/routes/+page.svelte
+++ web/pingpong/src/routes/+page.svelte
@@ -3,6 +3,7 @@
   import { page } from '$app/stores';
   import { Modal, Heading, P, Card } from 'flowbite-svelte';
   import CreateClass from '$lib/components/CreateClass.svelte';
+  import PageHeader from '$lib/components/PageHeader.svelte';
 
   export let data;
   export let form;
@@ -19,9 +20,7 @@
 </script>
 
 <div>
-  <header class="bg-blue-light-50 p-8 pb-6">
-    <Heading tag="h2" class="font-serif">Welcome to PingPong!</Heading>
-  </header>
+  <PageHeader />
   <div class="flex flex-wrap gap-4 p-8">
     {#each classes as cls}
       <Card horizontal class="w-80 h-40" href={`/class/${cls.id}`}>
