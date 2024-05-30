bdcf4d084aa2ec1f6e3c90e611f8f7f99b0cd603
Joe Nudell <jnudell@hks.harvard.edu>
2024-05-30 13:49:17 -0400

    editable profile fields

--- web/pingpong/src/routes/onboarding/+page.svelte
+++ web/pingpong/src/routes/onboarding/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { page } from '$app/stores';
-  import { Input, Button } from 'flowbite-svelte';
+  import { Input, Button, Heading } from 'flowbite-svelte';
   import * as api from '$lib/api';
   import { happyToast, sadToast } from '$lib/toast';
   import { goto } from '$app/navigation';
@@ -40,37 +40,46 @@
 </script>
 
 <div class="w-full h-full flex align-center justify-center">
-  <div class="m-auto w-96 bg-white rounded">
-    <header>
-      <h1>PingPong</h1>
-    </header>
-    <form on:submit={saveName}>
-      <section class="flex flex-col gap-2">
-        <div>
-          Welcome, {data.me.user?.email || 'Unknown'}.
-        </div>
-        <div>
-          <Input
-            type="text"
-            placeholder="First name / given name"
-            name="firstName"
-            value={data.me.user?.first_name || ''}
-            disabled={loading}
-          />
-        </div>
-        <div>
-          <Input
-            type="text"
-            placeholder="Surname / family name"
-            name="lastName"
-            value={data.me.user?.last_name || ''}
-            disabled={loading}
-          />
-        </div>
-        <div>
-          <Button type="submit" class="text-black" disabled={loading}>Continue to PingPong</Button>
-        </div>
-      </section>
-    </form>
+  <div class="m-auto w-[30rem] bg-white rounded-2xl">
+    <div class="bg-orange rounded-t-2xl">
+      <Heading tag="h1" customSize="text-4xl" class="px-16 py-8 w-full text-center text-white"
+        >PingPong.</Heading
+      >
+    </div>
+    <div class="px-8 py-4">
+      <form on:submit={saveName}>
+        <section class="flex flex-col gap-4">
+          <div class="w-full text-center">
+            Welcome, {data.me.user?.email || 'Unknown'}.
+          </div>
+          <div class="w-full text-center">Please enter your name to continue.</div>
+          <div>
+            <Input
+              type="text"
+              placeholder="First name / given name"
+              name="firstName"
+              value={data.me.user?.first_name || ''}
+              disabled={loading}
+            />
+          </div>
+          <div>
+            <Input
+              type="text"
+              placeholder="Surname / family name"
+              name="lastName"
+              value={data.me.user?.last_name || ''}
+              disabled={loading}
+            />
+          </div>
+          <div class="text-center">
+            <Button
+              type="submit"
+              class="text-white bg-orange rounded-full hover:bg-orange-dark"
+              disabled={loading}>Continue to PingPong</Button
+            >
+          </div>
+        </section>
+      </form>
+    </div>
   </div>
 </div>
--- web/pingpong/src/routes/profile/+page.svelte
+++ web/pingpong/src/routes/profile/+page.svelte
@@ -1,9 +1,42 @@
 <script lang="ts">
   import PageHeader from '$lib/components/PageHeader.svelte';
-  import { Heading } from 'flowbite-svelte';
+  import { Heading, Input, Label, Helper, Spinner } from 'flowbite-svelte';
   import dayjs from 'dayjs';
+  import * as api from '$lib/api';
 
   export let data;
+
+  const inputState = {
+    first_name: {
+      loading: false,
+      error: ''
+    },
+    last_name: {
+      loading: false,
+      error: ''
+    }
+  };
+
+  const saveField = (field: keyof typeof inputState) => async (event: Event) => {
+    const target = event.target as HTMLInputElement | undefined;
+    if (!target) {
+      return;
+    }
+    const value = target.value.trim();
+    if (!value) {
+      return;
+    }
+    inputState[field].loading = true;
+    inputState[field].error = '';
+    const response = await api.updateUserInfo(fetch, { [field]: value });
+    const expanded = api.expandResponse(response);
+    if (expanded.error) {
+      inputState[field].error = expanded.error.detail || 'Unknown error';
+    } else {
+      target.value = expanded.data[field]!;
+    }
+    inputState[field].loading = false;
+  };
 </script>
 
 <div class="flex flex-col gap-8 about h-full overflow-y-auto">
@@ -15,13 +48,43 @@
 
   <div class="mx-12 flex gap-12 flex-wrap">
     <div>
-      <Heading tag="h3">First Name</Heading>
-      <p>{data.me.user?.first_name || 'Unknown'}</p>
+      <Label for="firstName" color={inputState.first_name.error ? 'red' : undefined}
+        >First Name</Label
+      >
+      <Input
+        name="firstName"
+        color={inputState.first_name.error ? 'red' : undefined}
+        value={data.me.user?.first_name}
+        on:change={saveField('first_name')}
+      >
+        <div slot="right" class={inputState.first_name.loading ? '' : 'hidden'}>
+          <Spinner size="4" color="green" />
+        </div>
+      </Input>
+      {#if inputState.first_name.error}
+        <Helper color="red" class="mt-2">
+          <p>{inputState.first_name.error}</p>
+        </Helper>
+      {/if}
     </div>
 
     <div>
-      <Heading tag="h3">Last Name</Heading>
-      <p>{data.me.user?.last_name || 'Unknown'}</p>
+      <Label for="lastName" color={inputState.last_name.error ? 'red' : undefined}>Last Name</Label>
+      <Input
+        name="lastName"
+        color={inputState.last_name.error ? 'red' : undefined}
+        value={data.me.user?.last_name}
+        on:change={saveField('last_name')}
+      >
+        <div slot="right" class={inputState.last_name.loading ? '' : 'hidden'}>
+          <Spinner size="4" color="green" />
+        </div>
+      </Input>
+      {#if inputState.last_name.error}
+        <Helper color="red" class="mt-2">
+          <p>{inputState.last_name.error}</p>
+        </Helper>
+      {/if}
     </div>
   </div>
 
