ea9e213892013184bc415b3052b11aa667cb22d9
Evangelos Kassos <github@evangeloskassos.com>
2024-06-17 14:49:04 -0400

    240 / Better Handle Code Interpreter Output (#328)
    Better supports the `code_interpreter` tool calls with
    improved UI rendering.
    
    - Calls the correct full list of optimistic and server messages to
    determine when to create a new assistant message in #createToolCall, so
    that we don't append the `code_interpreter` tool output in a previous
    assistant message sent before the current user message.
    - Adds improved checks so that we don't create a new empty message for
    File Search calls (might want to display this somehow in the future)
    - Introduces separate Message Images and Code Interpreter Images Types
    for different display logic
    - Displays Code Interpreter Images as a collapsed AccordionItem
    - Displays Code Interpreter Code as an open AccordionItem with wrapping
    text (avoids trailing lines)

--- web/pingpong/src/lib/stores/thread.ts
+++ web/pingpong/src/lib/stores/thread.ts
@@ -465,19 +465,19 @@ export class ThreadManager {
    */
   #createToolCall(call: api.ToolCallDelta) {
     this.#data.update((d) => {
-      const messages = d.data?.messages;
+      const messages = get(this.messages);
       if (!messages?.length) {
         console.warn('Received a tool call without any messages.');
         return d;
       }
-      const sortedMessages = messages.sort((a, b) => b.created_at - a.created_at);
+      const sortedMessages = messages.sort((a, b) => b.data.created_at - a.data.created_at);
       const lastMessage = sortedMessages[0];
       if (!lastMessage) {
         console.warn('Received a tool call without a previous message.');
         return d;
       }
 
-      if (lastMessage.role !== 'assistant') {
+      if (lastMessage.data.role !== 'assistant' && call.type == 'code_interpreter') {
         d.data?.messages.push({
           role: 'assistant',
           content: [],
@@ -528,7 +528,10 @@ export class ThreadManager {
           for (const output of chunk.code_interpreter.outputs) {
             switch (output.type) {
               case 'image':
-                lastMessage.content.push({ type: 'image_file', image_file: output.image });
+                lastMessage.content.push({
+                  type: 'code_output_image_file',
+                  image_file: output.image
+                });
                 break;
               default:
                 console.warn('Unhandled tool call output', output);
