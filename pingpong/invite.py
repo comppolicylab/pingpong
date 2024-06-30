d20012f20eb35feeda346b98c44fac69ec1d4c49
Evangelos Kassos <github@evangeloskassos.com>
2024-06-30 04:42:29 -0400

    Lint

--- pingpong/invite.py
+++ pingpong/invite.py
@@ -12,16 +12,17 @@ async def send_invite(
     message = messageTemplate.substitute(
         {
             "title": f"You&#8217;re invited to join {invite.class_name} on PingPong.",
-            "subtitle": (f"{invite.inviter_name} has invited you"
-            if invite.inviter_name
-            else "You have been invited")
+            "subtitle": (
+                f"{invite.inviter_name} has invited you"
+                if invite.inviter_name
+                else "You have been invited"
+            )
             + "to join"
             + invite.class_name
-            + f" as {invite.formatted_role}"
-            if invite.formatted_role
-            else "" + " on PingPong. Click the link below to accept" + "their"
-            if invite.formatted_role
-            else "this" + "invitation.",
+            + (f" as {invite.formatted_role}" if invite.formatted_role else "")
+            + " on PingPong. Click the link below to accept"
+            + ("their" if invite.formatted_role else "this")
+            + "invitation.",
             "type": "invitation",
             "cta": f"Join {invite.class_name} on PingPong",
             "underline": "PingPong is a tool for using large language models in a group setting. PingPong is built on top of GPT-4, a large language model developed by OpenAI.",
