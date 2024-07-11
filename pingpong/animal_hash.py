a182a9148fb67db36b8744ffc3bd184456b6b86c
Evangelos Kassos <github@evangeloskassos.com>
2024-07-11 11:22:50 -0400

    Update functions

--- pingpong/animal_hash.py
+++ pingpong/animal_hash.py
@@ -1,27 +1,17 @@
-from .adjectives import adjectives
-from .animals import animals
-from hashlib import md5
-from .compress import compress
-import re
+from .names import adjectives, names
+from typing import TypeVar
+import mmh3
 
+T = TypeVar('T')
+
+def list_p(A: list[T], pct: float) -> T:
+    return A[int(pct * len(A))]
+
+uint64_max = 2**64
 
 def animal_hash(input: str) -> str:
-    """
-    Generate the anthropomorphic name given some input bytes.
-    :param input: The input bytes (strings must be encoded).
-    :return: The animal name in angry-purple-tiger format.
-    """
-    # take the md5 checksum of the input bytes
-    hex_digest = md5(input.encode()).hexdigest()
+    x, y = mmh3.hash64(input, signed=False)
 
-    # split the digest into byte pairs and convert to hex ints
-    p = re.compile("(..?)")
-    pairs = p.findall(hex_digest)
-    bytes = [int(x, base=16) for x in pairs]
-    compressed = compress(bytes, 2)
-
-    # use the compressed output as the indices for the adjective-color-animal format
-    adjective = adjectives[compressed[0]]
-    animal = animals[compressed[1]]
-
-    return f"{adjective.capitalize()} {animal.capitalize()}"
+    adj = list_p(adjectives, x / uint64_max)
+    animal = list_p(names, y / uint64_max)
+    return f"{adj.capitalize()} {animal.capitalize()}"
