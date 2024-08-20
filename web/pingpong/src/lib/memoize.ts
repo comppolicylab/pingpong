53b76fbf4a564fa8277d1d913b793a4bb9b393ee
Joe Nudell <jnudell@hks.harvard.edu>
2024-08-20 14:58:21 -0400

    honor latex rendering preference on the front-end

---
+++ web/pingpong/src/lib/memoize.ts
@@ -0,0 +1,23 @@
+/**
+ * Generate a key for the memoization cache.
+ */
+type KeyFn<T extends unknown[]> = (...args: T) => string;
+
+/**
+ * Reasonable key function to use for most inputs.
+ */
+const defaultKeyFn = (...args: unknown[]) => JSON.stringify(args);
+
+/**
+ * Wrap a function with a memoization cache.
+ */
+export const memoize = <S extends Array<unknown>, U, T extends (...args: S) => U>(fn: T, key: KeyFn<S> = defaultKeyFn): T => {
+  const cache = new Map<string, U>();
+  return ((...args: Parameters<T>) => {
+    const k = key(...args);
+    if (!cache.has(k)) {
+      cache.set(k, fn(...args));
+    }
+    return cache.get(k);
+  }) as T;
+}
