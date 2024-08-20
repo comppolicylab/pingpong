b09e336b545415f95521660cdb73d3b1ea565a5a
Joe Nudell <jnudell@hks.harvard.edu>
2024-08-20 15:36:41 -0400

    add and update tests

--- web/pingpong/src/lib/markdown.test.ts
+++ web/pingpong/src/lib/markdown.test.ts
@@ -44,9 +44,14 @@ const x = 1;
   });
 
   it('should render inline LaTeX with punctuation after it', () => {
-    expect(markdown(`This is $N$'s test.`)).toBe(
+    expect(markdown(`This is $N$'s test.`, {latex: true})).toBe(
       `<p>This is <span class="katex"><span class="katex-mathml"><math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mi>N</mi></mrow><annotation encoding="application/x-tex">N</annotation></semantics></math></span><span class="katex-html" aria-hidden="true"><span class="base"><span class="strut" style="height:0.6833em;"></span><span class="mord mathnormal" style="margin-right:0.10903em;">N</span></span></span></span>&#39;s test.</p>
 `
+    );
+  });
+
+  it('should not render inline LaTeX when LaTeX rendering is disabled', () => {
+    expect(markdown(`This is $N$'s test.`, {latex: false})).toBe(`<p>This is $N$&#39;s test.</p>\n`
     );
   });
 
@@ -56,7 +61,7 @@ const x = 1;
 Test Math
 ===
 Inline math: $\\frac{1}{2}$
-`)
+`, {latex: true})
     ).toMatchSnapshot();
   });
 
@@ -70,7 +75,7 @@ Here is Ohm's law:
 $$
 V = IR
 $$
-`)
+`, {latex: true})
     ).toMatchSnapshot();
   });
 
@@ -80,7 +85,7 @@ $$
 Test Math
 ===
 - Here is Ohm's law: $$V = IR$$
-`)
+`, {latex: true})
     ).toMatchSnapshot();
   });
 });
