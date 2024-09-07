comppolicylab/pingpong

commit 3cf47a8a7ef471e89fa588950f295b56db2bd710

Call DELETE Canvas OAuth2 endpoint on disconnect (#463)
Being a good citizen and letting Canvas know we won't be using a user
access token anymore when we disconnect a Canvas account.

pingpong/canvas.py
@@ -145,6 +145,7 @@ class CanvasCourseClient(ABC):
         self,
         buffer: int = 60,
         force_refresh: bool = False,
+        log_out: bool = False,
     ) -> str:
         """Get the Canvas access token for the class.
 
@@ -161,6 +162,10 @@ class CanvasCourseClient(ABC):
         # No Canvas access token is found for this class
         if not response.access_token:
             raise CanvasException("No Canvas access token for class")
+
+        # If we are simply logging the user out (killing the connection), return the current token
+        if log_out:
+            return response.access_token
 
         # Check if the user making the request is the user whose Canvas account is connected for the class
         if response.user_id != self.user_id:
@@ -201,6 +206,16 @@ class CanvasCourseClient(ABC):
                     refresh_token=response.get("refresh_token", ""),
                     expires_in=int(response["expires_in"]),
                 )
+
+    async def log_out(self) -> None:
+        async with aiohttp.ClientSession() as session:
+            token = await self._get_access_token(log_out=True)
+            params = {"access_token": token}
+            await session.delete(
+                self.config.url("/login/oauth2/token"),
+                data=params,
+                raise_for_status=True,
+            )
 
     async def create_response(
         self, resp: aiohttp.ClientResponse

pingpong/server.py
@@ -921,14 +921,35 @@ async def unlink_canvas_class(class_id: str, tenant: str, request: Request):
 )
 async def remove_canvas_connection(class_id: str, tenant: str, request: Request):
     canvas_settings = get_canvas_config(tenant)
-    userIds = await models.Class.remove_lms_sync(
-        request.state.db,
-        int(class_id),
-        canvas_settings.tenant,
-        schemas.LMSType(canvas_settings.type),
-        kill_connection=True,
-    )
-    await delete_canvas_permissions(request.state.authz, userIds, class_id)
+
+    async with LightweightCanvasClient(
+        canvas_settings, int(class_id), request
+    ) as client:
+        try:
+            await client.log_out()
+            userIds = await models.Class.remove_lms_sync(
+                request.state.db,
+                int(class_id),
+                canvas_settings.tenant,
+                schemas.LMSType(canvas_settings.type),
+                kill_connection=True,
+            )
+            await delete_canvas_permissions(request.state.authz, userIds, class_id)
+
+        except ClientResponseError as e:
+            logger.exception("delete_canvas_permissions: ClientResponseError occurred")
+            raise HTTPException(
+                status_code=e.code,
+                detail="Canvas returned an error when removing your account: "
+                + e.message,
+            )
+        except Exception:
+            logger.exception("delete_canvas_permissions: Exception occurred")
+            raise HTTPException(
+                status_code=500,
+                detail="We faced an internal error while removing your account.",
+            )
+
     return {"status": "ok"}
 
 

commit f0e9dcec9a3338a1fe9dd458874fe75bafce8d22

Canvas Sync: Option for other users to disconnect an account (#473)
Allows users whose Canvas account is not currently connected to the
PingPong group to remove said connection and add their own. Presents
option to keep or remove users that have been imported through Canvas.

Also fixes an error introduced in #463 where calling `DELETE` on the
Canvas OAuth2 endpoint on disconnect would result in a `401
Unauthorized` error if the token was already expired.

pingpong/canvas.py
@@ -145,7 +145,6 @@ class CanvasCourseClient(ABC):
         self,
         buffer: int = 60,
         force_refresh: bool = False,
-        log_out: bool = False,
     ) -> str:
         """Get the Canvas access token for the class.
 
@@ -162,10 +161,6 @@ class CanvasCourseClient(ABC):
         # No Canvas access token is found for this class
         if not response.access_token:
             raise CanvasException("No Canvas access token for class")
-
-        # If we are simply logging the user out (killing the connection), return the current token
-        if log_out:
-            return response.access_token
 
         # Check if the user making the request is the user whose Canvas account is connected for the class
         if response.user_id != self.user_id:
@@ -194,28 +189,27 @@ class CanvasCourseClient(ABC):
     async def _request_access_token(
         self, params: CanvasInitialAccessTokenRequest | CanvasRefreshAccessTokenRequest
     ) -> CanvasAccessToken:
-        async with aiohttp.ClientSession() as session:
-            async with session.post(
-                self.config.url("/login/oauth2/token"),
-                data=params.__dict__,
-                raise_for_status=True,
-            ) as resp:
-                response = await resp.json()
-                return CanvasAccessToken(
-                    access_token=response["access_token"],
-                    refresh_token=response.get("refresh_token", ""),
-                    expires_in=int(response["expires_in"]),
-                )
-
-    async def log_out(self) -> None:
-        async with aiohttp.ClientSession() as session:
-            token = await self._get_access_token(log_out=True)
-            params = {"access_token": token}
-            await session.delete(
-                self.config.url("/login/oauth2/token"),
-                data=params,
-                raise_for_status=True,
+        async with self.http_session.post(
+            self.config.url("/login/oauth2/token"),
+            data=params.__dict__,
+            raise_for_status=True,
+        ) as resp:
+            response = await resp.json()
+            return CanvasAccessToken(
+                access_token=response["access_token"],
+                refresh_token=response.get("refresh_token", ""),
+                expires_in=int(response["expires_in"]),
             )
+
+    @with_retry(max_retries=3)
+    async def log_out(self, retry_attempt: int = 0) -> None:
+        access_token = await self._get_access_token(force_refresh=retry_attempt > 0)
+        params = {"access_token": access_token}
+        await self.http_session.delete(
+            self.config.url("/login/oauth2/token"),
+            data=params,
+            raise_for_status=True,
+        )
 
     async def create_response(
         self, resp: aiohttp.ClientResponse

pingpong/server.py
@@ -909,10 +909,7 @@ async def sync_canvas_class(
     response_model=schemas.GenericStatus,
 )
 async def unlink_canvas_class(
-    class_id: str,
-    tenant: str,
-    request: Request,
-    keep_users: bool = True,
+    class_id: str, tenant: str, request: Request, keep_users: bool = True
 ):
     canvas_settings = get_canvas_config(tenant)
     userIds = await models.Class.remove_lms_sync(
@@ -932,7 +929,10 @@ async def unlink_canvas_class(
     response_model=schemas.GenericStatus,
 )
 async def remove_canvas_connection(
-    class_id: str, tenant: str, request: Request, keep_users: bool = True
+    class_id: str,
+    tenant: str,
+    request: Request,
+    keep_users: bool = True,
 ):
     canvas_settings = get_canvas_config(tenant)
 

web/pingpong/src/lib/components/CanvasDisconnectModal.svelte
@@ -1,0 +1,27 @@
+<script lang="ts">
+  import { Button } from 'flowbite-svelte';
+  import { ExclamationCircleOutline } from 'flowbite-svelte-icons';
+  import { createEventDispatcher } from 'svelte';
+
+  export let canvasCourseCode: string = '';
+  export let leftAction: string = 'Keep imported users';
+  export let rightAction: string = 'Remove imported users';
+
+  // Custom events
+  const dispatch = createEventDispatcher();
+</script>
+
+<div class="text-center">
+  <ExclamationCircleOutline class="mx-auto mb-4 text-red-600 w-12 h-12" />
+  <h3 class="mb-5 text-xl text-black font-bold">
+    Remove imported users from {canvasCourseCode || 'Canvas'}?
+  </h3>
+  <h4 class="mb-5 text-sm text-black font-normal">
+    While Canvas Sync was active, PingPong imported all users in your Canvas roster. If you keep
+    imported users, you can edit their roles or remove their access to this group at any time.
+  </h4>
+  <div class="flex flex-row gap-2 justify-center">
+    <Button pill color="alternative" on:click={() => dispatch('keep')}>{leftAction}</Button>
+    <Button pill color="red" on:click={() => dispatch('remove')}>{rightAction}</Button>
+  </div>
+</div>

web/pingpong/src/routes/group/[classId]/manage/+page.svelte
@@ -44,7 +44,6 @@
     SortHorizontalOutline,
     AdjustmentsHorizontalOutline,
     UserRemoveSolid,
-    ExclamationCircleOutline,
     FileLinesOutline
   } from 'flowbite-svelte-icons';
   import { sadToast, happyToast } from '$lib/toast';
@@ -56,6 +55,7 @@
   import DropdownContainer from '$lib/components/DropdownContainer.svelte';
   import CanvasClassDropdownOptions from '$lib/components/CanvasClassDropdownOptions.svelte';
   import PermissionsTable from '$lib/components/PermissionsTable.svelte';
+  import CanvasDisconnectModal from '$lib/components/CanvasDisconnectModal.svelte';
 
   /**
    * Application data.
@@ -456,6 +456,17 @@
     }
   };
 
+  const reconnectCanvasAccount = async () => {
+    const result = await api.removeCanvasConnection(fetch, data.class.id, 'harvard', true);
+    const response = api.expandResponse(result);
+    if (response.error) {
+      invalidateAll();
+      sadToast(response.error.detail || 'An unknown error occurred');
+    } else {
+      await redirectToCanvas();
+    }
+  };
+
   // The HTMLElement refs of the canvas class options.
   let classNodes: { [key: string]: HTMLElement } = {};
   // Clean up state on navigation. Invalidate data so that any changes
@@ -825,10 +836,21 @@
                 with this group. Once they have selected a course to sync with this group, PingPong will
                 automatically sync the course's roster.
               </p>
-              <p class="mt-2 text-sm">
-                Need to link your own account? Ask {data.class.lms_user?.name || 'them'} to disconnect
-                their Canvas account from this PingPong group.
+              <p class="mt-2 mb-4 text-sm">
+                Need to link your own account? You can disconnect their Canvas account from this
+                PingPong group.
               </p>
+              <div class="flex gap-2">
+                <Button
+                  pill
+                  size="xs"
+                  class="border border-amber-900 hover:bg-amber-900 text-amber-900 hover:bg-gradient-to-t hover:from-amber-800 hover:to-amber-700 hover:text-white"
+                  on:click={() => removeCanvasConnection(false)}
+                  on:touchstart={() => removeCanvasConnection(false)}
+                >
+                  <UserRemoveSolid class="w-4 h-4 me-2" />Disconnect Canvas account</Button
+                >
+              </div>
             </div>
           </Alert>
         {:else if data.class.lms_status === 'linked' && data.class.lms_user?.id && data.me.user?.id === data.class.lms_user?.id}
@@ -889,48 +911,18 @@
                     </div></DropdownItem
                   >
                   <Modal bind:open={disconnectCanvas} size="sm" autoclose>
-                    <div class="text-center">
-                      <ExclamationCircleOutline class="mx-auto mb-4 text-red-600 w-12 h-12" />
-                      <h3 class="mb-5 text-xl text-black font-bold">
-                        Remove imported users from {data.class.lms_class?.course_code || 'Canvas'}?
-                      </h3>
-                      <h4 class="mb-5 text-sm text-black font-normal">
-                        While Canvas Sync was active, PingPong imported all users in your Canvas
-                        roster. If you keep imported users, you can edit their roles or remove their
-                        access to this group at any time.
-                      </h4>
-                      <div class="flex flex-row gap-2 justify-center">
-                        <Button
-                          pill
-                          color="alternative"
-                          on:click={() => removeCanvasConnection(true)}>Keep imported users</Button
-                        >
-                        <Button pill color="red" on:click={() => removeCanvasConnection(false)}
-                          >Remove imported users</Button
-                        >
-                      </div>
-                    </div>
+                    <CanvasDisconnectModal
+                      canvasCourseCode={data.class.lms_class?.course_code || ''}
+                      on:keep={() => removeCanvasConnection(true)}
+                      on:remove={() => removeCanvasConnection(false)}
+                    />
                   </Modal>
                   <Modal bind:open={disconnectClass} size="sm" autoclose>
-                    <div class="text-center">
-                      <ExclamationCircleOutline class="mx-auto mb-4 text-red-600 w-12 h-12" />
-                      <h3 class="mb-5 text-xl text-black font-bold">
-                        Remove imported users from {data.class.lms_class?.course_code || 'Canvas'}?
-                      </h3>
-                      <h4 class="mb-5 text-sm text-black font-normal">
-                        While Canvas Sync was active, PingPong imported all users in your Canvas
-                        roster. If you keep imported users, you can edit their roles or remove their
-                        access to this group at any time.
-                      </h4>
-                      <div class="flex flex-row gap-2 justify-center">
-                        <Button pill color="alternative" on:click={() => deleteClassSync(true)}
-                          >Keep imported users</Button
-                        >
-                        <Button pill color="red" on:click={() => deleteClassSync(false)}
-                          >Remove imported users</Button
-                        >
-                      </div>
-                    </div>
+                    <CanvasDisconnectModal
+                      canvasCourseCode={data.class.lms_class?.course_code || ''}
+                      on:keep={() => deleteClassSync(true)}
+                      on:remove={() => deleteClassSync(false)}
+                    />
                   </Modal>
                 </Dropdown>
               </div>
@@ -954,11 +946,32 @@
                   ? dayjs.utc(data.class.lms_last_synced).fromNow()
                   : 'never'}
               </p>
-              <p class="mt-2 text-sm">
+              <p class="mt-2 mb-4 text-sm">
                 {data.class.lms_user?.name || 'Someone in your course'} has linked their Canvas account
-                with this group. Need to link your own account? Ask {data.class.lms_user?.name ||
-                  'them'} to disconnect their Canvas account from this PingPong group.
+                with this group. Need to link your own account? You can disconnect their Canvas account
+                from this PingPong group.
+                <span class="font-bold"
+                  >This action is irreversible and will delete all imported users from Canvas.</span
+                >
               </p>
+              <div class="flex gap-2">
+                <Button
+                  pill
+                  size="xs"
+                  class="border border-green-900 hover:bg-green-900 text-green-900 hover:bg-gradient-to-t hover:from-green-800 hover:to-green-700 hover:text-white"
+                  on:click={() => (disconnectCanvas = true)}
+                  on:touchstart={() => (disconnectCanvas = true)}
+                >
+                  <UserRemoveSolid class="w-4 h-4 me-2" />Disconnect Canvas account</Button
+                >
+              </div>
+              <Modal bind:open={disconnectCanvas} size="sm" autoclose>
+                <CanvasDisconnectModal
+                  canvasCourseCode={data.class.lms_class?.course_code || ''}
+                  on:keep={() => removeCanvasConnection(true)}
+                  on:remove={() => removeCanvasConnection(false)}
+                />
+              </Modal>
             </div>
           </Alert>
         {:else if data.class.lms_status === 'error' && data.class.lms_user?.id && data.me.user?.id === data.class.lms_user?.id}
@@ -983,8 +996,8 @@
                   pill
                   size="xs"
                   class="border border-red-900 bg-gradient-to-t from-red-800 to-red-700 text-white hover:from-red-700 hover:to-red-600"
-                  on:click={redirectToCanvas}
-                  on:touchstart={redirectToCanvas}
+                  on:click={reconnectCanvasAccount}
+                  on:touchstart={reconnectCanvasAccount}
                 >
                   <RefreshOutline class="w-4 h-4 me-2" />Reconnect Canvas account</Button
                 >
@@ -1011,10 +1024,32 @@
                   ? dayjs.utc(data.class.lms_last_synced).fromNow()
                   : 'never'}
               </p>
-              <p class="mt-2 text-sm">
-                Need to link your own account? Ask {data.class.lms_user?.name || 'them'} to disconnect
-                their Canvas account from this PingPong group.
+              <p class="mt-2 mb-4 text-sm">
+                {data.class.lms_user?.name || 'Someone in your course'} has linked their Canvas account
+                with this group. Need to link your own account? You can disconnect their Canvas account
+                from this PingPong group.
+                <span class="font-bold"
+                  >This action is irreversible and will delete all imported users from Canvas.</span
+                >
               </p>
+              <div class="flex gap-2">
+                <Button
+                  pill
+                  size="xs"
+                  class="border border-green-900 hover:bg-green-900 text-green-900 hover:bg-gradient-to-t hover:from-green-800 hover:to-green-700 hover:text-white"
+                  on:click={() => (disconnectCanvas = true)}
+                  on:touchstart={() => (disconnectCanvas = true)}
+                >
+                  <UserRemoveSolid class="w-4 h-4 me-2" />Disconnect Canvas account</Button
+                >
+              </div>
+              <Modal bind:open={disconnectCanvas} size="sm" autoclose>
+                <CanvasDisconnectModal
+                  canvasCourseCode={data.class.lms_class?.course_code || ''}
+                  on:keep={() => removeCanvasConnection(true)}
+                  on:remove={() => removeCanvasConnection(false)}
+                />
+              </Modal>
             </div>
           </Alert>
         {/if}