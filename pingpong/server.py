c3aa474c5ceb0323afcd294de222f50ec8e0d001
Evangelos Kassos <github@evangeloskassos.com>
2024-09-13 10:48:29 -0400

    Patch: Streaming threads may fail when `image` content is provided (#504)
    Adds a check to make sure we have `text` content type before trying to
    access annotations.

--- pingpong/server.py
+++ pingpong/server.py
@@ -1447,7 +1447,7 @@ async def get_thread(
 
     for message in messages.data:
         for content in message.content:
-            if content.type and content.type == "text" and content.text.annotations:
+            if content.type == "text" and content.text.annotations:
                 for annotation in content.text.annotations:
                     if annotation.type == "file_citation":
                         annotation.file_citation.file_name = file_names.get(
