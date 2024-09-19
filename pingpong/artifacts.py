8ce8972c7a42a491411f60a704c2dc568075869f
Evangelos Kassos <github@evangeloskassos.com>
2024-09-19 15:54:59 -0400

    Move session init

--- pingpong/artifacts.py
+++ pingpong/artifacts.py
@@ -3,7 +3,6 @@ import logging
 import os
 
 from abc import ABC, abstractmethod
-from datetime import datetime, timedelta
 from typing import IO, TextIO
 
 logger = logging.getLogger(__name__)
@@ -20,19 +19,15 @@ class S3ArtifactStore(BaseArtifactStore):
     def __init__(self, bucket: str, expiry: int = 43_200):
         self._bucket = bucket
         self._expiry = expiry
-        self._session = aioboto3.Session()
 
     async def put(self, name: str, content: IO, content_type: str) -> str:
         content.seek(0)
-        async with self._session.client("s3") as s3:
+        async with aioboto3.Session().client("s3") as s3:
             await s3.put_object(
                 Bucket=self._bucket,
                 Key=name,
                 Body=content.read(),
                 ContentType=content_type,
-                Expires=datetime.now()
-                + timedelta(seconds=self._expiry)
-                + timedelta(hours=1),
                 ContentDisposition=f'attachment; filename="{name}"',
             )
             return await s3.generate_presigned_url(
