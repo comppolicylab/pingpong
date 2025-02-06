comppolicylab/pingpong

commit fc1b42e80a5edb5977695b045ef9fedbc38071f1

[Canvas Sync] Handle Missing Emails or Invalid Tokens (#722)
- Adds new `CanvasInvalidTokenException`, which is raised when we cannot
receive a refreshed token through `/login/oauth2/token`. When there is a
`ClientResponseError`, `with_retry` will force refresh the
authentication token.
- Adds support for raising custom errors for specific
`ClientResponseError`s in `with_retry`. We do not want to specify this
within `with_retry` to keep it generic enough.
- Canvas Sync will now check that we receive emails and enrollment
information for users when verifying access to course rosters and before
attempting to create `UserClassRole`s when syncing. Classes will be
marked as having a sync error in both cases.
- Handle log outs more gracefully: If we don't find the token
(`CanvasInvalidTokenException`) we can ignore the error.
- New `Spinner`s in the UI to account for retries when logging out from
Canvas.

pingpong/canvas.py
@@ -45,6 +45,10 @@ class CanvasAccessException(Exception):
         self.detail = detail
 
 
+class CanvasInvalidTokenException(Exception):
+    pass
+
+
 class CanvasWarning(Exception):
     def __init__(self, detail: str = "", code: int | None = None):
         self.code = code
@@ -112,6 +116,7 @@ class CanvasCourseClient(ABC):
         self.user_id = user_id
         self.nowfn = nowfn
         self.missing_sso_ids = False
+        self.missing_user_information = False
         self.sync_without_sso_ids = sync_without_sso_ids
 
     async def __aenter__(self):
@@ -218,7 +223,7 @@ class CanvasCourseClient(ABC):
                 expires_in=int(response["expires_in"]),
             )
 
-    @with_retry(max_retries=3)
+    @with_retry(max_retries=3, raise_custom_errors={400: CanvasInvalidTokenException()})
     async def log_out(self, retry_attempt: int = 0) -> None:
         access_token = await self._get_access_token(
             force_refresh=retry_attempt > 0, log_out=True
@@ -252,7 +257,7 @@ class CanvasCourseClient(ABC):
             next_page=next_page,
         )
 
-    @with_retry(max_retries=3)
+    @with_retry(max_retries=3, raise_custom_errors={400: CanvasInvalidTokenException()})
     async def _make_authed_request_post(
         self,
         path: str,
@@ -273,7 +278,7 @@ class CanvasCourseClient(ABC):
         ) as resp:
             return await self.create_response(resp)
 
-    @with_retry(max_retries=3)
+    @with_retry(max_retries=3, raise_custom_errors={400: CanvasInvalidTokenException()})
     async def _make_authed_request_get(
         self,
         path: str,
@@ -482,6 +487,19 @@ class CanvasCourseClient(ABC):
                         code=403,
                         detail="You do not have permission to access SIS information for this class. Please ask another privileged user to set up Canvas Sync. If you're still facing issues, contact your Canvas administrator.",
                     )
+                if not user.get("email") or not (
+                    user.get("enrollments") and len(user["enrollments"]) > 0
+                ):
+                    raise CanvasException(
+                        code=403,
+                        detail="You do not have permission to access email or enrollment information for this class. Please ask another privileged user to set up Canvas Sync. If you're still facing issues, contact your Canvas administrator.",
+                    )
+                if not user.get("enrollments"):
+                    raise CanvasException(
+                        code=403,
+                        detail="You do not have permission to access enrollment information for this class. Please ask another privileged user to set up Canvas Sync. If you're still facing issues, contact your Canvas administrator.",
+                    )
+
                 return True
         return False
 
@@ -572,11 +590,17 @@ class CanvasCourseClient(ABC):
         """
 
         for user in data:
-            is_teacher = False
-            for enrollment in user["enrollments"]:
-                if enrollment["type"] in ["TeacherEnrollment", "TaEnrollment"]:
-                    is_teacher = True
-                    break
+            # Check that the user has email and enrollment information
+            if not user.get("email") or not (
+                user.get("enrollments") and len(user["enrollments"]) > 0
+            ):
+                logging.warning(
+                    f"User {user['id']} does not have an email or enrollment information in the Canvas response. Marking class as having a sync error."
+                )
+                self.missing_user_information = True
+                break
+
+            # Check that the user has an SSO ID if required
             if self.config.sso_target and not user.get(self.config.sso_target):
                 logging.warning(
                     f"User {user['email']} does not have an SSO ID in the Canvas response. Marking class as having a sync error."
@@ -584,6 +608,15 @@ class CanvasCourseClient(ABC):
                 self.missing_sso_ids = True
                 if not self.sync_without_sso_ids:
                     break
+
+            # Calculate user permissions
+            is_teacher = False
+            for enrollment in user["enrollments"]:
+                if enrollment["type"] in ["TeacherEnrollment", "TaEnrollment"]:
+                    is_teacher = True
+                    break
+
+            # Create UserClassRole object for the user
             yield CreateUserClassRole(
                 email=user["email"],
                 sso_id=user.get(self.config.sso_target),
@@ -647,11 +680,11 @@ class CanvasCourseClient(ABC):
             lms_type=self.config.type,
             sso_tenant=self.config.sso_tenant,
         )
-        if self.missing_sso_ids:
+        if self.missing_sso_ids or self.missing_user_information:
             await Class.mark_lms_sync_error(self.db, self.class_id)
-            if not self.sync_without_sso_ids:
+            if not self.sync_without_sso_ids or self.missing_user_information:
                 self._raise_sync_error_if_manual()
-                return
+            return
 
         await self._update_user_roles()
         await Class.update_last_synced(self.db, self.class_id)
@@ -699,7 +732,7 @@ class ManualCanvasClient(CanvasCourseClient):
     def _raise_sync_error_if_manual(self):
         raise CanvasException(
             code=403,
-            detail="Some users in the Canvas class do not have SIS information. Please ask another privileged user to set up Canvas Sync. If you're still facing issues, contact your Canvas administrator.",
+            detail="Some users in the Canvas class do not have email, enrollment, or SIS information. Please ask another privileged user to set up Canvas Sync. If you're still facing issues, contact your Canvas administrator.",
         )
 
 
@@ -795,6 +828,11 @@ async def canvas_sync_all(
                     sync_without_sso_ids=sync_without_sso_ids,
                 ) as client:
                     await client.sync_roster()
+            except CanvasInvalidTokenException:
+                logger.exception(
+                    f"Canvas access token for class {class_.id} is invalid. Marking class as having a sync error."
+                )
+                await Class.mark_lms_sync_error(session, class_.id)
             except Exception as e:
-                logger.error(f"Error syncing class {class_.id}: {e}", exc_info=True)
+                logger.exception(f"Error syncing class {class_.id}: {e}")
                 await session_.rollback()

pingpong/__main__.py
@@ -470,7 +470,7 @@ def sync_pingpong_with_lms(crontime: str, host: str, port: int) -> None:
                 await _lms_sync_all()
                 logger.info(f"Sync completed successfully at {datetime.now()}")
             except Exception as e:
-                logger.error(f"Error during sync: {e}")
+                logger.exception(f"Error during sync: {e}")
 
     # Run the Uvicorn server in the background
     with server.run_in_thread():
@@ -570,11 +570,11 @@ async def _send_activity_summaries(
                     await session.commit()
 
                 except GetOpenAIClientException as e:
-                    logger.error(f"Error getting OpenAI client: {e.detail}")
+                    logger.exception(f"Error getting OpenAI client: {e.detail}")
                     no_errors = False
                     continue
                 except Exception as e:
-                    logger.error(f"Error sending class summary: {e}")
+                    logger.exception(f"Error sending class summary: {e}")
                     no_errors = False
                     continue
 
@@ -615,7 +615,7 @@ def run_dynamic_tasks_with_args(host: str, port: int, tasks: list[str]) -> None:
         Execute a given task based on its name, cron schedule, and provided arguments.
         """
         if function_name not in FUNCTIONS_MAP:
-            logger.error(f"Function '{function_name}' is not recognized.")
+            logger.exception(f"Function '{function_name}' is not recognized.")
             sys.exit(1)
 
         func = FUNCTIONS_MAP[function_name]
@@ -626,9 +626,7 @@ def run_dynamic_tasks_with_args(host: str, port: int, tasks: list[str]) -> None:
                     f"Task '{task_name}' (calling {function_name}) completed successfully at {datetime.now()}"
                 )
             except Exception as e:
-                logger.error(
-                    f"Error in task '{task_name}' ({function_name}): {e}", exc_info=True
-                )
+                logger.exception(f"Error in task '{task_name}' ({function_name}): {e}")
 
     async def _parse_tasks():
         task_coroutines = []

pingpong/artifacts.py
@@ -55,7 +55,7 @@ class S3ArtifactStore(BaseArtifactStore):
                 async for chunk in s3_object["Body"].iter_chunks(chunk_size=chunk_size):
                     yield chunk
             except ClientError as e:
-                logger.error(f"Error streaming file {name}: {e}")
+                logger.exception(f"Error streaming file {name}: {e}")
                 raise ArtifactStoreError(
                     code=500, detail=f"Error downloading thread export: {str(e)}"
                 )
@@ -90,5 +90,5 @@ class LocalArtifactStore(BaseArtifactStore):
                 while chunk := f.read(chunk_size):
                     yield chunk
         except Exception as e:
-            logger.error(f"Error streaming file {name}: {e}")
+            logger.exception(f"Error streaming file {name}: {e}")
             raise ArtifactStoreError(code=500, detail=f"Error reading file: {str(e)}")

pingpong/config.py
@@ -296,7 +296,7 @@ def _load_config() -> Config:
     try:
         return Config.model_validate(tomllib.loads(_raw_cfg))
     except Exception as e:
-        logger.error(f"Error loading config: {e}")
+        logger.exception(f"Error loading config: {e}")
         raise
 
 

pingpong/retry.py
@@ -1,19 +1,19 @@
 import asyncio
 import random
-from typing import Any, Mapping, Callable
-
 from aiohttp import ClientResponseError
+from typing import Any, Mapping, Callable
 
 
 def with_retry(
     max_retries: int = 5,
     max_delay: int = 60,
     backoff: int = 2,
+    raise_custom_errors: dict[int, Exception] = {},
 ) -> Callable:
     def decorator(f: Callable) -> Callable:
         async def wrapper(*args: Any, **kwargs: Mapping[str, Any]) -> Any:
             attempt = 0
-            last_error: Exception | None = None
+            last_error: ClientResponseError | None = None
             while attempt < max_retries:
                 try:
                     return await f(*args, retry_attempt=attempt, **kwargs)
@@ -29,6 +29,8 @@ def with_retry(
                 except Exception as e:
                     raise e
             if last_error:
+                if last_error.status in raise_custom_errors:
+                    raise raise_custom_errors[last_error.status]
                 raise last_error
 
         return wrapper

pingpong/scripts/__main__.py
@@ -37,7 +37,7 @@ def sync_pingpong_with_airtable(crontime: str, host: str, port: int) -> None:
                 await _process_external_logins_to_add()
                 logger.info(f"Sync completed successfully at {datetime.now()}")
             except Exception as e:
-                logger.error(f"Error during sync: {e}")
+                logger.exception(f"Error during sync: {e}")
 
     # Run the Uvicorn server in the background
     with server.run_in_thread():

pingpong/server.py
@@ -67,6 +67,7 @@ from .config import config
 from .canvas import (
     CanvasAccessException,
     CanvasException,
+    CanvasInvalidTokenException,
     CanvasWarning,
     LightweightCanvasClient,
     ManualCanvasClient,
@@ -1134,6 +1135,13 @@ async def get_canvas_classes(class_id: str, tenant: str, request: Request):
             raise HTTPException(
                 status_code=e.code, detail="Canvas returned an error: " + e.message
             ) from e
+        except CanvasInvalidTokenException:
+            await models.Class.mark_lms_sync_error(request.state.db, int(class_id))
+            logger.exception("get_canvas_classes: CanvasInvalidTokenException occurred")
+            raise HTTPException(
+                status_code=401,
+                detail="Your Canvas token is invalid. Please reconnect to Canvas. If the problem persists, please contact us.",
+            )
         except CanvasException as e:
             logger.exception("get_canvas_classes: CanvasException occurred")
             raise HTTPException(
@@ -1212,6 +1220,15 @@ async def verify_canvas_class_permissions(
             raise HTTPException(
                 status_code=e.code, detail="Canvas returned an error: " + e.message
             )
+        except CanvasInvalidTokenException:
+            await models.Class.mark_lms_sync_error(request.state.db, int(class_id))
+            logger.exception(
+                "verify_canvas_class_permissions: CanvasInvalidTokenException occurred"
+            )
+            raise HTTPException(
+                status_code=401,
+                detail="Your Canvas token is invalid. Please reconnect to Canvas. If the problem persists, please contact us.",
+            )
         except Exception:
             logger.exception("verify_canvas_class_permissions: Exception occurred")
             raise HTTPException(
@@ -1267,6 +1284,13 @@ async def update_canvas_class(
             raise HTTPException(
                 status_code=e.code, detail="Canvas returned an error: " + e.message
             )
+        except CanvasInvalidTokenException:
+            await models.Class.mark_lms_sync_error(request.state.db, int(class_id))
+            logger.exception("sync_canvas_class: CanvasInvalidTokenException occurred")
+            raise HTTPException(
+                status_code=401,
+                detail="Your Canvas token is invalid. Please reconnect to Canvas. If the problem persists, please contact us.",
+            )
         except Exception:
             logger.exception("update_canvas_class: Exception occurred")
             raise HTTPException(
@@ -1318,6 +1342,13 @@ async def sync_canvas_class(
                 status_code=e.code or 500,
                 detail=e.detail or "We faced an error while syncing with Canvas.",
             )
+        except CanvasInvalidTokenException:
+            await models.Class.mark_lms_sync_error(request.state.db, int(class_id))
+            logger.exception("sync_canvas_class: CanvasInvalidTokenException occurred")
+            raise HTTPException(
+                status_code=401,
+                detail="Your Canvas token is invalid. Please reconnect to Canvas. If the problem persists, please contact us.",
+            )
         except Exception:
             logger.exception("sync_canvas_class: Exception occurred")
             raise HTTPException(
@@ -1361,12 +1392,37 @@ async def remove_canvas_connection(
     keep_users: bool = True,
 ):
     canvas_settings = get_canvas_config(tenant)
-
     async with LightweightCanvasClient(
         canvas_settings, int(class_id), request
     ) as client:
         try:
             await client.log_out()
+        except ClientResponseError as e:
+            logger.exception("delete_canvas_permissions: ClientResponseError occurred")
+            raise HTTPException(
+                status_code=e.code,
+                detail="Canvas returned an error when removing your account: "
+                + e.message,
+            )
+        except CanvasInvalidTokenException:
+            logger.warning(
+                "delete_canvas_permissions: CanvasInvalidTokenException occurred",
+                exc_info=True,
+            )
+        except CanvasException as e:
+            logger.exception("delete_canvas_permissions: CanvasException occurred")
+            raise HTTPException(
+                status_code=e.code or 500,
+                detail="We faced an error while removing your account: " + e.detail,
+            )
+        except Exception:
+            logger.exception("delete_canvas_permissions: Exception occurred")
+            raise HTTPException(
+                status_code=500,
+                detail="We faced an internal error while removing your account.",
+            )
+
+        try:
             userIds = await models.Class.remove_lms_sync(
                 request.state.db,
                 int(class_id),
@@ -1376,31 +1432,11 @@ async def remove_canvas_connection(
                 keep_users=keep_users,
             )
             await delete_canvas_permissions(request.state.authz, userIds, class_id)
-
-        except ClientResponseError as e:
-            logger.exception("delete_canvas_permissions: ClientResponseError occurred")
-            raise HTTPException(
-                status_code=e.code,
-                detail="Canvas returned an error when removing your account: "
-                + e.message,
+            logger.info(
+                f"Canvas account removed from PingPong class {class_id} by user {request.state.session.user.id}."
             )
-        except CanvasException as e:
-            logger.exception("delete_canvas_permissions: CanvasException occurred")
-            raise HTTPException(
-                status_code=e.code or 500,
-                detail="We faced an error while removing your account: " + e.detail,
-            )
-        except CanvasWarning as e:
-            logger.warning(
-                "delete_canvas_permissions: CanvasWarning occurred: %s", e.detail
-            )
-            raise HTTPException(
-                status_code=e.code or 500,
-                detail=e.detail
-                or "We faced an error while getting your Canvas classes.",
-            ) from e
         except Exception:
-            logger.exception("delete_canvas_permissions: Exception occurred")
+            logger.exception("remove_canvas_connection: Exception occurred")
             raise HTTPException(
                 status_code=500,
                 detail="We faced an internal error while removing your account.",
@@ -2371,7 +2407,7 @@ async def get_last_run(
                 last_run.id, thread_id=thread.thread_id
             )
         except openai.APIConnectionError as e:
-            logger.error("Error connecting to OpenAI: %s", e)
+            logger.exception("Error connecting to OpenAI: %s", e)
             # Continue polling
 
     return {"thread": thread, "run": last_run}
@@ -2607,7 +2643,9 @@ async def create_thread(
             ),
         )
     except openai.InternalServerError as e:
-        logger.error("Error creating thread: %s", e.body.get("message") or e.message)
+        logger.exception(
+            "Error creating thread: %s", e.body.get("message") or e.message
+        )
         if vector_store_id:
             await openai_client.beta.vector_stores.delete(vector_store_id)
         raise HTTPException(
@@ -2615,7 +2653,9 @@ async def create_thread(
             detail="OpenAI was unable to process your request. If the issue persists, check PingPong's status page for updates.",
         )
     except openai.APIError as e:
-        logger.error("Error creating thread: %s", e.body.get("message") or e.message)
+        logger.exception(
+            "Error creating thread: %s", e.body.get("message") or e.message
+        )
         if vector_store_id:
             await openai_client.beta.vector_stores.delete(vector_store_id)
         raise HTTPException(
@@ -2651,7 +2691,7 @@ async def create_thread(
 
         return result
     except Exception as e:
-        logger.error("Error creating thread: %s", e)
+        logger.exception("Error creating thread: %s", e)
         if vector_store_id:
             await openai_client.beta.vector_stores.delete(vector_store_id)
         await openai_client.beta.threads.delete(thread.id)

pingpong/summary.py
@@ -205,9 +205,7 @@ async def send_class_summary_to_class_users(
             await session.commit()
 
         except Exception as e:
-            logger.error(
-                f"Failed to send summary to user {ucr.user_id}: {e}", exc_info=True
-            )
+            logger.exception(f"Failed to send summary to user {ucr.user_id}: {e}")
             no_errors = False
             continue
 

commit f364cf3e70b345e4f8e284ca4f616947eb22004e

Canvas Sync Roster Check might generate false negatives (#746)
Fixes an issue introduced in #722 where users may mistakenly see an
error that their Canvas connection does not contain enrollment
information.

pingpong/canvas.py
@@ -478,7 +478,15 @@ class CanvasCourseClient(ABC):
         # https://canvas.instructure.com/doc/api/users.html#User
         request_url = f"/api/v1/courses/{course_id}/users"
 
-        async for result in self._request_all_pages(request_url):
+        # Request Parameters:
+        # - include[]:
+        #   - term: Optional information to include with each Course.
+        #           When term is given, the information for the enrollment
+        #           term for each course is returned.
+
+        params = {"include[]": ["enrollments"]}
+
+        async for result in self._request_all_pages(request_url, params=params):
             if not result:
                 return False
             for user in result: