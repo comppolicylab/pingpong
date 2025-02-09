50858daf8ea198b7aef3fa60804852ef0c308ec0
Evangelos Kassos <github@evangeloskassos.com>
2025-02-09 12:05:34 -0500

    Feature

--- web/pingpong/src/lib/components/ThreadHeader.svelte
+++ web/pingpong/src/lib/components/ThreadHeader.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-  import { Button, Dropdown, DropdownItem } from 'flowbite-svelte';
+  import { Button, Dropdown, DropdownItem, Search } from 'flowbite-svelte';
   import { ChevronDownOutline, ArrowRightOutline, CogSolid } from 'flowbite-svelte-icons';
   import * as api from '$lib/api';
   import PageHeader, { mainTextClass } from './PageHeader.svelte';
+  import { goto } from '$app/navigation';
 
   export let classes: api.Class[];
   export let isOnClassPage: boolean;
@@ -10,6 +11,14 @@
   export let canManage: boolean = false;
 
   $: sortedClasses = classes.sort((a: api.Class, b: api.Class) => a.name.localeCompare(b.name));
+  let searchTerm = '';
+  $: filteredClasses = sortedClasses.filter((class_) => class_.name.toLowerCase().indexOf(searchTerm?.toLowerCase()) !== -1);
+  
+  let classDropdownOpen = false;
+  const goToClass = async (clsId: number) => {
+    classDropdownOpen = false;
+    await goto(`/group/${clsId}`);
+  };
 </script>
 
 <PageHeader>
@@ -22,11 +31,15 @@
         class="bg-white rounded-full ml-4 h-8 w-8 inline-block text-orange"
       /></Button
     >
-    <Dropdown class="w-64 overflow-y-auto py-1 min-h-0 max-h-[400px]">
-      {#each sortedClasses as cls}
+    <Dropdown class="w-64 overflow-y-auto py-1 min-h-0 max-h-[400px]" bind:open={classDropdownOpen}>
+      <div slot="header" class="w-64 p-3">
+        <Search size="md" bind:value={searchTerm}/>
+      </div>
+      {#each filteredClasses as cls}
         <DropdownItem
           class="flex items-center text-base font-semibold gap-4 py-4 text-sm tracking-wide font-medium uppercase hover:bg-blue-light-50"
-          href={`/group/${cls.id}`}>{cls.name}</DropdownItem
+          on:click={() => goToClass(cls.id)}
+          >{cls.name}</DropdownItem
         >
       {/each}
     </Dropdown>
