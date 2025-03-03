176b3883fcd799f7bbc2737b614818d9918b7695
Evangelos Kassos <github@evangeloskassos.com>
2025-03-03 17:54:18 -0500

    Patch: In `on_tool_call_created`, `tool_call` is of type `Dict`, not the expected type `ToolCall`

--- pingpong/ai.py
+++ pingpong/ai.py
@@ -321,7 +321,9 @@ class BufferedStreamHandler(openai.AsyncAssistantEventHandler):
         self.enqueue(
             {
                 "type": "tool_call_created",
-                "tool_call": tool_call.model_dump(),
+                "tool_call": tool_call
+                if isinstance(tool_call, Dict)
+                else tool_call.model_dump(),
             }
         )
 
@@ -498,7 +500,7 @@ async def run_thread(
                 pass
     except (ValueError, Exception) as e:
         try:
-            logger.warning(f"Error adding new thread message: {e}")
+            logger.exception(f"Error adding new thread message: {e}")
             yield orjson.dumps({"type": "presend_error", "detail": str(e)}) + b"\n"
         except Exception as e_:
             logger.exception(f"Error writing to stream: {e_}")
