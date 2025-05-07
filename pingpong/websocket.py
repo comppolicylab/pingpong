558c1b852e43d70ad021640af741520e7c49aa8a
Evangelos Kassos <github@evangeloskassos.com>
2025-05-07 08:40:57 -0400

    feat(voice)/ Add `far_field` noise reduction (#874)

--- pingpong/websocket.py
+++ pingpong/websocket.py
@@ -231,6 +231,7 @@ def ws_with_realtime_connection(func):
                             "type": "semantic_vad",
                             "eagerness": "high",
                         },
+                        "input_audio_noise_reduction": {"type": "far_field"},
                         "instructions": conversation_instructions,
                     }
                 )
