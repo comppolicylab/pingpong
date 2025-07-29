8720a564fbac729e1c193da189e075048fb00b61
Evangelos Kassos <github@evangeloskassos.com>
2025-07-29 15:33:31 -0400

    fix/ Log ExternalLogin RID messages under single message

--- pingpong/canvas.py
+++ pingpong/canvas.py
@@ -898,8 +898,7 @@ async def canvas_sync_all(
     sync_classes_with_error_status: bool = False,
 ) -> None:
     logger.info(
-        "Last ExternalLogin RID before sync: ",
-        await ExternalLogin.get_last_row_id(session),
+        f"Last ExternalLogin RID before sync: {await ExternalLogin.get_last_row_id(session)}"
     )
 
     async for class_ in Class.get_all_to_sync(
@@ -910,8 +909,7 @@ async def canvas_sync_all(
     ):
         logger.info(f"Syncing class {class_.id}...")
         logger.info(
-            f"ExternalLogin RID before class {class_.id} sync: ",
-            await ExternalLogin.get_last_row_id(session),
+            f"ExternalLogin RID before class {class_.id} sync: {await ExternalLogin.get_last_row_id(session)}"
         )
         async with session.begin_nested() as session_:
             try:
@@ -933,8 +931,7 @@ async def canvas_sync_all(
                 logger.exception(f"Error syncing class {class_.id}: {e}")
                 await session_.rollback()
         logger.info(
-            f"ExternalLogin RID after class {class_.id} sync: ",
-            await ExternalLogin.get_last_row_id(session),
+            f"ExternalLogin RID after class {class_.id} sync: {await ExternalLogin.get_last_row_id(session)}"
         )
     # Finally, commit all changes
     await session.commit()
