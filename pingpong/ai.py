9a76f8d62052325797d65cd821d48d721f46ee69
Evangelos Kassos <github@evangeloskassos.com>
2025-08-26 20:24:24 -0400

    Do not yield

--- pingpong/ai.py
+++ pingpong/ai.py
@@ -1760,7 +1760,7 @@ async def run_response(
                         restore_to_pending_if_queued=True,
                     )
                     await stream.close()
-                    yield handler.flush()
+                    return
             except openai.APIError as openai_error:
                 if openai_error.type == "server_error":
                     try:
