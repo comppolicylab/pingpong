c559e248da70109c8333f1300b650e21cf16de15
Evangelos Kassos <github@evangeloskassos.com>
2025-11-24 16:02:20 -0500

    fix

--- pingpong/ai.py
+++ pingpong/ai.py
@@ -1517,6 +1517,8 @@ class BufferedResponseStreamHandler:
                     "output_index": self.prev_output_index,
                     "type": "code_interpreter",
                     "code_interpreter": {"input": data.code or "", "outputs": None},
+                    "status": data.status,
+                    "run_id": str(self.run_id),
                 },
             }
         )
