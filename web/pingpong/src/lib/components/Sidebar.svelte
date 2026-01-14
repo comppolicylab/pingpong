5689db3439e5d8a9654fe7fae087c6999e2ab504
Evangelos Kassos <github@evangeloskassos.com>
2026-01-13 23:44:26 -0500

    fix: update assistants and assistantMetadata to reactive statements

--- web/pingpong/src/lib/components/Sidebar.svelte
+++ web/pingpong/src/lib/components/Sidebar.svelte
@@ -82,7 +82,7 @@
   $: currentClassId = parseInt($page.params.classId ?? '', 10);
   $: currentAssistantIdQuery = parseInt($page.url.searchParams.get('assistant') || '0', 10);
   $: currentAssistantId = $page.data.threadData?.thread?.assistant_id || currentAssistantIdQuery;
-  let assistants = ($page.data.assistants || []) as api.Assistant[];
+  $: assistants = ($page.data.assistants || []) as api.Assistant[];
   $: assistants.sort((a, b) => {
     // First sort by endorsement.
     if (a.endorsed && !b.endorsed) return -1;
@@ -107,7 +107,7 @@
   } else {
     assistantsToShow = assistants;
   }
-  let assistantMetadata = assistants.reduce(
+  $: assistantMetadata = assistants.reduce(
     (acc: Record<number, ReturnType<typeof getAssistantMetadata>>, assistant) => {
       acc[assistant.id] = getAssistantMetadata(assistant);
       return acc;
