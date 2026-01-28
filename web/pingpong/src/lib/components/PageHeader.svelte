a622ca13f4539d7ae21dea2d8f9d701dc88e82cc
Evangelos Kassos <github@evangeloskassos.com>
2026-01-28 14:13:27 -0500

    Lint

--- web/pingpong/src/lib/components/PageHeader.svelte
+++ web/pingpong/src/lib/components/PageHeader.svelte
@@ -6,7 +6,7 @@
 <header
 	class="items-center justify-between bg-blue-light-50 p-2 pt-6 pb-6 sm:flex sm:pb-2 lg:p-8 lg:pt-10 lg:pb-4"
 >
-	<div class="w-full min-w-0 sm:w-2/3">
+	<div class="w-full min-w-0 sm:max-w-2/3">
 		<slot name="left" />
 	</div>
 	<div class="mt-4 ml-2 shrink-0 sm:mt-0 sm:ml-0">
