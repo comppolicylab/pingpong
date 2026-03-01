b5a5094c4f9b8a095b27f9c123f3bfcc93ed9fe1
Evangelos Kassos <github@evangeloskassos.com>
2026-02-28 22:15:27 -0500

    chore/ resolve CodeQL's js/superfluous-trailing-arguments
    ## Internal
    ### Updates & Improvements
    - Update TypeScript code to resolve CodeQL's js/superfluous-trailing-arguments.

--- web/pingpong/src/lib/streams.ts
+++ web/pingpong/src/lib/streams.ts
@@ -1,46 +1,42 @@
 /**
  * A stream that parses chunks as JSON objects.
  */
-export class JSONStream extends TransformStream<string, object> {
-	constructor() {
-		super({
-			transform(chunk, controller) {
-				try {
-					controller.enqueue(JSON.parse(chunk));
-				} catch (e) {
-					controller.error(e);
-				}
+export function JSONStream(): TransformStream<string, object> {
+	return new TransformStream({
+		transform(chunk, controller) {
+			try {
+				controller.enqueue(JSON.parse(chunk));
+			} catch (e) {
+				controller.error(e);
 			}
-		});
-	}
+		}
+	});
 }
 
 /**
  * A stream that emits lines of text in chunks as they are received.
  */
-export class TextLineStream extends TransformStream<string, string> {
-	constructor() {
-		let buffer = '';
+export function TextLineStream(): TransformStream<string, string> {
+	let buffer = '';
 
-		super({
-			transform(chunk, controller) {
-				// Add characters to the buffer, emitting at each newline
-				for (const char of chunk) {
-					if (char === '\n') {
-						controller.enqueue(buffer);
-						buffer = '';
-					} else {
-						buffer += char;
-					}
-				}
-			},
-			flush(controller) {
-				// If there's anything left in the buffer, emit it
-				if (buffer.length > 0) {
+	return new TransformStream({
+		transform(chunk, controller) {
+			// Add characters to the buffer, emitting at each newline
+			for (const char of chunk) {
+				if (char === '\n') {
 					controller.enqueue(buffer);
 					buffer = '';
+				} else {
+					buffer += char;
 				}
 			}
-		});
-	}
+		},
+		flush(controller) {
+			// If there's anything left in the buffer, emit it
+			if (buffer.length > 0) {
+				controller.enqueue(buffer);
+				buffer = '';
+			}
+		}
+	});
 }
